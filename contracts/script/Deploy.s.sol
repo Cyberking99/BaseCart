1c18e715b7cf9304c15bccd0745a7a86ed46ff72
Kefas Kingsley <kefasiceking@gmail.com>
2025-11-09 06:02:21 +0100

    fix: add remapping for libraries

---
+++ contracts/script/Deploy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console} from "forge-std/Script.sol";
+import {BaseCartFactory} from "../src/BaseCartFactory.sol";
+
+contract DeplyScript is Script {
+    BaseCartFactory public baseCartFactory;
+
+    function setUp() public {}
+
+    function run() public {
+    	uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+
+        baseCartFactory = new BaseCartFactory();
+        console.log("BaseCartFactory deployed at: ", address(baseCartFactory));
+        vm.stopBroadcast();
+    }
+}
