47a6f91a04a7d91089f85e4dfd1aa36485d6ef06
morelucks <luckykamshak@gmail.com>
2025-12-11 19:30:03 +0100

    test: add createStore tests for event emission and multiple stores

--- contracts/test/BaseCartFactory.t.sol
+++ contracts/test/BaseCartFactory.t.sol
@@ -61,5 +61,42 @@ contract BaseCartFactoryTest is Test {
         assertEq(store.storeUrl(), STORE_URL, "Store URL should match");
         assertEq(store.storeDescription(), STORE_DESCRIPTION, "Store description should match");
     }
+
+    /**
+     * @dev Test that StoreCreated event is emitted
+     */
+    function test_CreateStore_EmitsEvent() public {
+        vm.expectEmit(true, true, false, true);
+        emit StoreCreated(user1, address(0), STORE_NAME, STORE_URL);
+
+        vm.prank(user1);
+        factory.createStore(STORE_NAME, STORE_URL, STORE_DESCRIPTION);
+    }
+
+    /**
+     * @dev Test that multiple stores can be created by same user
+     */
+    function test_CreateStore_Success_MultipleStores() public {
+        vm.startPrank(user1);
+        address store1 = factory.createStore("Store 1", "store-1", "Description 1");
+        address store2 = factory.createStore("Store 2", "store-2", "Description 2");
+        address store3 = factory.createStore("Store 3", "store-3", "Description 3");
+        vm.stopPrank();
+
+        // Verify all stores are registered
+        assertTrue(factory.isValidStore(store1), "Store 1 should be valid");
+        assertTrue(factory.isValidStore(store2), "Store 2 should be valid");
+        assertTrue(factory.isValidStore(store3), "Store 3 should be valid");
+
+        // Verify total stores count
+        assertEq(factory.getTotalStores(), 3, "Total stores should be 3");
+
+        // Verify stores by owner
+        address[] memory stores = factory.getStoresByOwner(user1);
+        assertEq(stores.length, 3, "User1 should have 3 stores");
+        assertEq(stores[0], store1, "First store should match");
+        assertEq(stores[1], store2, "Second store should match");
+        assertEq(stores[2], store3, "Third store should match");
+    }
 }
 
